34c8eff0b31c0e9d6946e8df7f5cf1b92fd78a79
Femi <pasival14@gmail.com>
2025-07-23 21:04:53 +0100

    update port

--- app.py
+++ app.py
@@ -197,4 +197,6 @@ def clear_session():
     return jsonify({'success': True})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
